d633d3fe0ac24bc2dca95179cb25913df82b599d
Matheus Gomes <21205109+gomesmat@users.noreply.github.com>
2021-08-26 22:13:11 -0300

    First

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      
+      <h1>Matheus Gomes</h1>
+      <h2>Desenvolvedor Front-end</h2>
+      <h3>contato@gomesmat.me</h3>
     </div>
   );
 }
-
-export default App;
