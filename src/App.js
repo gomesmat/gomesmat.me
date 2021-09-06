55816025b70ae55e104088b47f1d02aaa4bac6f6
Matheus Gomes <matheusribeirogomes@hotmail.com>
2021-09-05 22:26:37 -0300

    aguenta firme

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Matheus Gomes</h1>
+    <div className="App" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection:'column'}}>
+      {/* <h1>Matheus Gomes</h1>
       <h2>Desenvolvedor Front-end</h2>
-      <h3>contato@gomesmat.me</h3>
+      <h3>contato@gomesmat.me</h3> */}
+      <h2>aguenta firme aí</h2>
+      <img src="https://s3.amazonaws.com/image-products/96399/96399-1024.jpg" style={{height: '700px', marginTop: '1rem'}} />
+
     </div>
   );
 }
