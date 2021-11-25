2b22c054c63c11cda814d6133d2a5644f2f439b7
Matheus Gomes <21205109+gomesmat@users.noreply.github.com>
2021-11-25 02:36:34 -0300

    fix css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection:'column'}}>
+    <div className="App">
       {/* <h1>Matheus Gomes</h1>
       <h2>Desenvolvedor Front-end</h2>
       <h3>contato@gomesmat.me</h3> */}
